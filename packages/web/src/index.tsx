9a5ca02d7ebc6eef656cb2f783381830f569fada
kilbot <paul@kilbot.com.au>
2019-03-18 14:38:44 +0800

    add WooCommercePOS.ttf font

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,6 +2,7 @@ import { AppRegistry } from 'react-native-web';
 import App from '@wcpos/common/src/app';
 
 import './index.css';
+import '@wcpos/common/src/fonts/fonts.css';
 
 // register the app
 AppRegistry.registerComponent('wcpos', () => App);
