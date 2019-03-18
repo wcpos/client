069fa2d2c92eb598b28b544387bb40e9baabdf1f
kilbot <paul@kilbot.com.au>
2019-03-19 01:41:32 +0800

    add splash screen

--- packages/mobile/android/app/src/main/java/com/wcpos/client/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/wcpos/client/MainApplication.java
@@ -3,6 +3,7 @@ package com.wcpos.client;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new SplashScreenReactPackage(),
         new RNGestureHandlerPackage(),
         new WatermelonDBPackage()
       );
