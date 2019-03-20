feeb658bc6458667c77a3fe03632d361838d2f86
kilbot <paul@kilbot.com.au>
2019-03-20 23:42:13 +0800

    add storybook

---
+++ packages/storybook/.storybook/config.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+import { ThemeProvider } from 'styled-components/native';
+import { defaultTheme } from '@wcpos/common/src/lib/theme';
+import '@wcpos/common/src/fonts/fonts.css';
+
+const stories = require.context('../stories', true, /\.stories\.(js|jsx|tsx)$/);
+const common = require.context('../../common/src/', true, /\.stories\.(js|jsx|tsx)$/);
+
+function loadStories() {
+	addDecorator(story => <ThemeProvider theme={defaultTheme}>{story()}</ThemeProvider>);
+	addDecorator(withKnobs);
+	addDecorator(withInfo);
+	stories.keys().forEach(filename => stories(filename));
+	common.keys().forEach(filename => common(filename));
+}
+
+configure(loadStories, module);
