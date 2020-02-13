6c0817164bdaa329de22ba0338a2fcf75885bdc8
kilbot <paul@kilbot.com.au>
2020-02-13 15:07:09 +0400

    update deps

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -5,6 +5,7 @@ const {
 	addBabelPlugins,
 	babelExclude,
 	addBabelPresets,
+	addWebpackResolve,
 } = require('customize-cra');
 const path = require('path');
 
@@ -18,15 +19,36 @@ module.exports = override(
 	addWorkerLoader(),
 	addDecoratorsLegacy(),
 	...addBabelPlugins(
+		// 'babel-plugin-react-native-web',
+		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-optional-chaining',
 		'@babel/plugin-proposal-nullish-coalescing-operator'
+		// '@babel/plugin-proposal-export-default-from'
 	),
 	// ...addBabelPresets([]),
 	babelInclude([
 		// tell Babel to include common files
 		path.resolve('src'),
 		path.resolve('../common/src'),
-		// path.resolve('../../node_modules/@react-navigation'),
+		path.resolve('../../node_modules/react-native-screens'),
+		path.resolve('../../node_modules/react-native-reanimated'),
+		path.resolve('../../node_modules/react-native-gesture-handler'),
 	])
-	// babelExclude([])
+	// babelExclude([]),
+	// addWebpackResolve({
+	// 	alias: {
+	// 		'@react-navigation/bottom-tabs': path.resolve(
+	// 			'../../node_modules/@react-navigation/bottom-tabs/src'
+	// 		),
+	// 		'@react-navigation/compat': path.resolve('../../node_modules/@react-navigation/compat/src'),
+	// 		'@react-navigation/core': path.resolve('../../node_modules/@react-navigation/core/src'),
+	// 		'@react-navigation/drawer': path.resolve('../../node_modules/@react-navigation/drawer/src'),
+	// 		'@react-navigation/native-stack': path.resolve(
+	// 			'../../node_modules/@react-navigation/native-stack/src'
+	// 		),
+	// 		'@react-navigation/native': path.resolve('../../node_modules/@react-navigation/native/src'),
+	// 		'@react-navigation/routers': path.resolve('../../node_modules/@react-navigation/routers/src'),
+	// 		'@react-navigation/stack': path.resolve('../../node_modules/@react-navigation/stack/src'),
+	// 	},
+	// })
 );
