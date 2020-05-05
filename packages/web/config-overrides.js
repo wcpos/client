41fd3a158ef5f0b5b71d111db345b579dcaf4bba
kilbot <paul@kilbot.com.au>
2020-05-05 20:15:09 +0400

    update launchscreen

--- packages/web/config-overrides.js
+++ packages/web/config-overrides.js
@@ -3,9 +3,7 @@ const {
 	babelInclude,
 	addDecoratorsLegacy,
 	addBabelPlugins,
-	babelExclude,
-	addBabelPresets,
-	addWebpackResolve,
+	addWebpackAlias,
 } = require('customize-cra');
 const path = require('path');
 
@@ -25,7 +23,6 @@ module.exports = override(
 		'@babel/plugin-proposal-nullish-coalescing-operator'
 		// '@babel/plugin-proposal-export-default-from'
 	),
-	// ...addBabelPresets([]),
 	babelInclude([
 		// tell Babel to include common files
 		path.resolve('src'),
@@ -34,19 +31,7 @@ module.exports = override(
 		path.resolve('../../node_modules/react-native-reanimated'),
 		path.resolve('../../node_modules/react-native-gesture-handler'),
 	]),
-	// babelExclude([]),
-	addWebpackResolve({
-		alias: {
-			'react-native-linear-gradient': 'react-native-web-linear-gradient',
-			// '@react-navigation/compat': path.resolve('../../node_modules/@react-navigation/compat/src'),
-			// '@react-navigation/core': path.resolve('../../node_modules/@react-navigation/core/src'),
-			// '@react-navigation/drawer': path.resolve('../../node_modules/@react-navigation/drawer/src'),
-			// '@react-navigation/native-stack': path.resolve(
-			// 	'../../node_modules/@react-navigation/native-stack/src'
-			// ),
-			// '@react-navigation/native': path.resolve('../../node_modules/@react-navigation/native/src'),
-			// '@react-navigation/routers': path.resolve('../../node_modules/@react-navigation/routers/src'),
-			// '@react-navigation/stack': path.resolve('../../node_modules/@react-navigation/stack/src'),
-		},
+	addWebpackAlias({
+		'react-native-linear-gradient': 'react-native-web-linear-gradient',
 	})
 );
