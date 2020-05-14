42d69e1014c2e7c5a02d1057179d81dcc93492e6
kilbot <paul@kilbot.com.au>
2020-05-14 19:42:32 +0400

    update deps

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+const { defaults } = require('jest-config');
+console.log(defaults);
+
 module.exports = {
 	// All imported modules in your tests should be mocked automatically
 	// automock: false,
@@ -66,7 +69,7 @@ module.exports = {
 	// ],
 
 	// An array of file extensions your modules use
-	moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+	// moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
 	// moduleNameMapper: {},
