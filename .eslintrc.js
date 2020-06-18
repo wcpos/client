3d3f91e45f1fd3ae9273b8fe7108b6816030e2a4
kilbot <paul@kilbot.com.au>
2020-06-18 12:38:08 +0400

    add svg mock for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,9 @@ module.exports = {
 		'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
 		'no-underscore-dangle': ['error', { allowAfterThis: true }],
 		'react/prop-types': 'off',
-		'react/destructuring-assignment:': 'off',
+		'react/destructuring-assignment': 'off',
+		'react/jsx-props-no-spreading': 'off',
+		'react/static-property-placement': 'off',
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/jsx-wrap-multilines': [
--- jest.config.js
+++ jest.config.js
@@ -71,7 +71,9 @@ module.exports = {
 	// moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
 
 	// A map from regular expressions to module names that allow to stub out resources with a single module
-	// moduleNameMapper: {},
+	moduleNameMapper: {
+		'\\.svg$': '<rootDir>/packages/common/jest/__mocks__/svgr-mock.js',
+	},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
 	// modulePathIgnorePatterns: [],
