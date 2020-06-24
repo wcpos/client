0e56beb0cf8dfe74e10c8882814e69602311b5f2
kilbot <paul@kilbot.com.au>
2020-06-24 17:36:18 +0400

    fix sentry for android

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -2,7 +2,6 @@ import { AppRegistry } from 'react-native-web';
 import App from '@wcpos/common/src/app';
 
 import './index.css';
-import '@wcpos/common/src/fonts/fonts.css';
 
 // register the app
 AppRegistry.registerComponent('wcpos', () => App);
