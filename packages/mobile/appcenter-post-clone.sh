18651b3f5b176a8a62be67758313d2f79970ef8c
kilbot <paul@kilbot.com.au>
2020-07-09 12:13:18 +0400

    change to pre-build script for appcenter fix

--- packages/mobile/appcenter-post-clone.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# https://github.com/microsoft/appcenter/issues/1285#issuecomment-549373098
-# npm i @react-native-community/cli-platform-android
---
+++ packages/mobile/appcenter-pre-build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# https://github.com/microsoft/appcenter/issues/1285#issuecomment-549373098
+GRADLE_FILE=${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android/native_modules.gradle
+
+if [ -e "${GRADLE_FILE}" ]
+then
+    echo "##[warning][Pre-Build Action] - native_modules.gradle file found - ok!"
+else
+    echo "##[error][Pre-Build Action] - native_modules.gradle file not found!"
+fi 
+
+npm i @react-native-community/cli-platform-android
+
+if [ -e "${GRADLE_FILE}" ]
+then
+    echo "##[warning][Pre-Build Action] - native_modules.gradle file found - ok!"
+else
+    echo "##[error][Pre-Build Action] - native_modules.gradle file not found!"
+fi 
