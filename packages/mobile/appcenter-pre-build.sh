48b53521ee017978934c9d4f6638cf02523e2546
kilbot <paul@kilbot.com.au>
2020-07-09 12:53:38 +0400

    appcenter fix

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -3,16 +3,7 @@
 # https://github.com/microsoft/appcenter/issues/1285#issuecomment-549373098
 GRADLE_FILE=${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android/native_modules.gradle
 
-if [ -e "${GRADLE_FILE}" ]
-then
-    echo "##[warning][Pre-Build Action] - native_modules.gradle file found - ok!"
-else
-    echo "##[error][Pre-Build Action] - native_modules.gradle file not found!"
-	ls ${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community
-	ls ${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android
-fi 
-
-yarn add @react-native-community/cli-platform-android
+yarn install
 
 if [ -e "${GRADLE_FILE}" ]
 then
