072ae5b05629c84b0b6fcea2554cda14652794da
kilbot <paul@kilbot.com.au>
2021-04-06 14:11:04 +0200

    Update appcenter-pre-build.sh

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
 # https://github.com/microsoft/appcenter/issues/1285#issuecomment-549373098
-GRADLE_FILE=${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android/native_modules.gradle
-LOCAL_CLI=${APPCENTER_SOURCE_DIRECTORY}/packages/mobile/node_modules/react-native/local-cli/cli.js
+ANDROID_CLI=${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android/native_modules.gradle
+IOS_CLI=${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-ios/build/index.js
+LOCAL_CLI=${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native/local-cli/cli.js
 
 yarn install
 
-if [ -e "${GRADLE_FILE}" ]
+if [ -e "${ANDROID_CLI}" ]
 then
-    echo "##[info][Pre-Build Action] - native_modules.gradle file found - ok!"
+    echo "##[info][Pre-Build Action] - android cli found - ok!"
 else
-    echo "##[error][Pre-Build Action] - native_modules.gradle file not found!"
+    echo "##[error][Pre-Build Action] - android cli not found!"
 	# ls ${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/
     # ls ${APPCENTER_SOURCE_DIRECTORY}/node_modules/@react-native-community/cli-platform-android
 fi 
 
+if [ -e "${IOS_CLI}" ]
+then
+    echo "##[info][Pre-Build Action] - ios cli found - ok!"
+else
+    echo "##[error][Pre-Build Action] - ios cli not found!"
+fi 
+
 if [ -e "${LOCAL_CLI}" ]
 then
-    echo "##[info][Pre-Build Action] - react-native/local-cli/cli.js file found - ok!"
+    echo "##[info][Pre-Build Action] - local cli found - ok!"
 else
     # should add @react-native-community/cli as a dependency
-    echo "##[error][Pre-Build Action] - react-native/local-cli/cli.js file not found!"
+    echo "##[error][Pre-Build Action] - local cli not found!"
 fi 
